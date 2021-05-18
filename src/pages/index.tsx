fcb5a312768a652b99938e82175e5577830b7b62
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-05-18 12:23:07 -0300

    working with components in nextjs

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from "./home.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> World
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
+    </>
+  );
+}
